8106686e4b59cba3a32db9ec659a5e1c79ea550f
windAtlantis <ufofre@gmail.com>
2018-08-07 23:24:25 +0800

    DEV:SpringBoot系列测试代码 1.login失败后path前缀是user导致资源加载异常

--- src/main/java/com/atw/test/springbootproj/controllers/LoginController.java
+++ src/main/java/com/atw/test/springbootproj/controllers/LoginController.java
@@ -3,9 +3,9 @@ package com.atw.test.springbootproj.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
-import java.util.Map;
 
 /**
  * @author Karl
@@ -18,13 +18,14 @@ public class LoginController {
 	@PostMapping("/user/login")
 	public String userlogin(@RequestParam("form-username") String username,
 	                        @RequestParam("form-password") String passwd,
-	                        Map<String, Object> map, HttpSession session) {
+	                        RedirectAttributes redirectAttributes, HttpSession session) {
 		if ("admin".equals(username) && "admin".equals(passwd)) {
 			session.setAttribute("loginid", username);
 			return "redirect:/hello";
 		} else {
-			map.put("msg", "登陆失败");
-			return "login";
+			// 可以在 login 对应的模版中通过表达式，比如 jsp 中 jstl 用 ${param}，获取返回值。该值其实是保存在 session 中的，并且会在下次重定向请求时删除。
+			redirectAttributes.addFlashAttribute("msg", "登陆失败");
+			return "redirect:/login";
 		}
 	}
 }
